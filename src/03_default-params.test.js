e860344183abe80202673cd9f321ae2958462936
Lewis Williams <lewiswilliams1501@gmail.com>
2019-04-01 14:03:40 +0100

    Day 2

---
+++ src/03_default-params.test.js
@@ -0,0 +1,38 @@
+// 03: Default parameters - basics
+// To do: make all tests pass, leave the expect lines unchanged!
+
+describe('default parameters make function parameters more flexible', () => {
+
+  it('define it using an assignment to the parameter `function(param=1){}`', () => {
+    let number = (int = 0) => int
+
+    expect(number()).toEqual(0)
+  })
+
+  it('it is not used when a value is given', () => {
+    function xhr(method = null) {
+      return method
+    }
+
+    expect(xhr('POST')).toEqual('POST')
+  })
+
+  it('it is evaluated at run time', () => {
+    let defaultValue
+    function xhr(method = `value: ${defaultValue = 42}`) {
+      return method
+    }
+
+    expect(xhr()).toEqual('value: 42')
+  })
+
+  it('it can also be a function', () => {
+    let defaultValue = () => {}
+    function fn(value = defaultValue()) {
+      return value
+    }
+
+    expect(fn()).toEqual(defaultValue())
+  })
+
+})
